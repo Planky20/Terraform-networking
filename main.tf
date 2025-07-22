c5a2833d9d9d4c18b1faa140a40dd1fb1d9fe187
Władysław Lastowski <plankton.by@gmail.com>
2025-07-22 16:48:15 +0200

    Created Linux VMs in each VNET/subnet

--- main.tf
+++ main.tf
@@ -18,3 +18,14 @@ module "network" {
   resource_prefix              = each.key
   depends_on                   = [module.resource-group]
 }
+
+module "virtual-machines" {
+  for_each                            = var.environment # One machine for each VNET
+  source                              = "./modules/compute/virtualMachines"
+  resource_group_name                 = var.resource_group_name
+  location                            = var.location
+  virtual_machine_count               = each.value.virtual_machine_count
+  virtual_network_interface_ids       = module.network[each.key].virtual_network_interfaces_ids # From the output of the network module
+  virtual_machine_public_ip_addresses = module.network[each.key].public_ip_addresses            # From the output of the network module
+  resource_prefix                     = each.key
+}
